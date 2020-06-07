a5d4799c8b4e5d0551f3b276f0896a5d6bfb9765
jiapeng <1319019326@qq.com>
2020-06-07 12:30:51 +0800

    lianxi

--- lianxi/jenkins/clean_workspace.py
+++ lianxi/jenkins/clean_workspace.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__file__)
 
 
 def get_jenkins_instance():
-    jenkins_url = "http://192.168.1.103:22"
+    jenkins_url = "http://192.168.1.1"
     jenkins_username = "xx"
     jenkins_password = "xx"
     print(Jenkins(jenkins_url, username=jenkins_username, password=jenkins_password))
